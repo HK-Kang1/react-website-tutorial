bbbc9b2bd42c78089b79d4bf172a71526e26cbcd
Heemin Kang <heeminkang1@gmail.com>
2025-01-24 13:17:56 -0700

    card component

---
+++ my-react-app/src/Card.jsx
@@ -0,0 +1,13 @@
+
+function Card(){
+    return(
+        <div className="card">
+            <img className="card-image" src="https://placehold.co/150" alt="Profile Picture"></img>
+            <h2 className="card-title">Bro Code</h2>
+            <p className="card-text">I make Youtube videos and play video games</p>
+        </div>
+    );
+
+}
+
+export default Card
