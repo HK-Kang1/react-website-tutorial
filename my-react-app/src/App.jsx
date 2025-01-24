86b695516dd7a20f0f39e3aae554aecb49c2cd5e
Heemin Kang <heeminkang1@gmail.com>
2025-01-24 13:47:34 -0700

    add props

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,10 +1,13 @@
-import Card from './Card.jsx'
+import Student from './Student.jsx'
 
 function App() {
     return(
         <>
-            <Card></Card>
-            <Card></Card>
+            <Student name="Spongebob" age={30} isStudent={true}></Student>
+            <Student name="Patrick" age={42} isStudent={false}></Student>
+            <Student name="Squidward" age={50} isStudent={false}></Student>
+            <Student name="Sandy" age={32} isStudent={true}></Student>
+            <Student name="Larry"></Student>
         </>
     );
 
